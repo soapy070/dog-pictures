88c6a7550841f32980e57a6a011e5e41fb871aff
Iain Watson <iain@watson234.karoo.co.uk>
2023-02-16 14:30:31 +0000

    saving all files after creating dogs.js

---
+++ dogs.js
@@ -0,0 +1,35 @@
+/*
+Author: Iain Watson
+Web page about dogs
+*/
+
+//Variables
+const breedListUrl = "https://dog.ceo/api/breeds/list/all";
+const breedList = document.getElementById("breed-list");
+
+//when the page loads
+window.addEventListener("load", );
+
+//retrieve the list of all breeds from API
+function getBreedList(){
+    return fetch(breedListUrl).then(response => response.json());
+}
+
+//add breed to drop down list
+function updateBreedList(){
+    getBreedList().then(function(data){
+            //get breed name
+            for(element in data.message){
+                //append to the select list
+                let option = createOption(element);
+                breedList.appendChild(option);
+            
+        }
+    }
+}
+
+function createOption(text){
+    let option = document.createElement("option");
+    option.textContent = text;
+    return option;
+}
